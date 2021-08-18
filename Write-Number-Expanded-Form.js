d2c9bc2201286d71e079e9e5efa7b28ede6fb865
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-08-18 11:29:25 -0300

    add kata unsolved yet.. fckin-hard

--- Write-Number-Expanded-Form.js
+++ Write-Number-Expanded-Form.js
@@ -1,20 +1,56 @@
 function expandedForm(num) {
-  if (num > 99) {
-    const centena = Math.round(num / 100) * 100;
-    const dezena = Math.round((num - centena) / 10) * 10;
-    const unidade = num % 10;
-    return `${centena} + ${dezena} + ${unidade}`;
+  let numStr = num.toString().split('');
+  
+  for (let i = 0 ; i < numStr.length; i++ ) {
+    for (let y = numStr.length - i; y > 1; y--) {
+      numStr[i] += '0'; 
+    }
   }
-  const dezena = Math.round(num / 10) * 10;
-  const unidade = num % 10;
-  return `${dezena} + ${unidade}`;
+
+  if (num > 1000 & num <= 99999) {
+    const trueAnswer = numStr.map((num2) => {
+      if (num2[0] !== '0' && num2[-1] !== '0') {
+        return num2
+      } 
+    })
+    return trueAnswer.toString().replace(/,,/g, ' ').split(' ').join(' + ')
+  } else if (num > 99999 & num <= 999999) {
+    const trueAnswer = numStr.map((num2) => {
+      if (num2[0] !== '0' && num2[-1] !== '0') {
+        return num2
+      } 
+    })
+    return trueAnswer.toString().replace(/,/g, ' ').split(' ').join(' + ')
+  } else if (num > 999999) {
+    const trueAnswer = numStr.map((num2,index) => {
+      if (num2[0] !== '0' && num2[-1] !== '0') {
+        return num2
+      }
+    })
+    const x = trueAnswer.toString().replace(/,/g, ' ').split(' ').join(' + ')
+  }
+
+  return numStr.join(' + ')
 }
 
-console.log(expandedForm(99));
+  console.log(expandedForm(420370022))
 
 
-// UNFINISHED YET ---------------*
+// UNFINISHED YET TO BIGZIG NUMBERS ---------------*
 
 // expandedForm(12); // Should return '10 + 2'
 // expandedForm(42); // Should return '40 + 2'
 // expandedForm(70304); // Should return '70000 + 300 + 4'
+
+
+// teste codewars: 
+// '90000000000000 + 2000000000000 + 90000000000 + 3000000000 + 400000000 + 3000000 + 30000 + 4000 + 500 + 70 + 3'
+
+// Expected: '4000000 + 900000 + 80000 + 2000 + 300 + 40 + 2', = 4982342
+// instead got: '4000000,900000,80000,2000,300,40,2'
+
+// Expected: '400000000 + 20000000 + 300000 + 70000 + 20 + 2', = 420370022
+// instead got: '400000000 + 20000000 +  + 300000 + 70000 +  +  + 20 + 2'
+
+// Expected: '9000000',
+// instead got: '9000000 +  +  +  +  +  + '
